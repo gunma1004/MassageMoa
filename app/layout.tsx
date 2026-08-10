331aca4f4edf315ecc466951c769d72ce69ce1be
gunma1004 <hanabank94@naver.com>
2026-08-10 11:02:20 +0900

    메타테그 변경

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,17 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "건마사랑 | 서울·경기·인천 24시 출장마사지 홈케어 추천",
-  description: "서울, 경기, 인천 전지역 25분 내 신속 방문! 선입금 0원 100% 후불제 안심 예약. 스웨디시, 타이, 아로마 마사지 및 프라이빗 힐링 서비스 제공.",
+  title: "건마사랑 | 서울·경기·인천 24시 출장마사지 추천 1등 플랫폼",
+  // 80자 이내로 다듬은 페이지 설명 (공백 포함 76자)
+  description: "서울, 경기, 인천 전지역 25분 내 신속 방문! 선입금 없는 100% 후불제 안심 예약. 타이, 아로마, 스웨디시 프리미엄 힐링 서비스 제공.",
   
-  // 🔍 구글 & 네이버 소유 확인 태그 추가 위치
+  openGraph: {
+    title: "건마사랑 | 서울·경기·인천 24시 출장마사지 추천",
+    description: "선입금 없는 100% 후불 안심 케어! 수도권 전지역 25분 내 빠른 방문 마사지 정보를 확인하세요.",
+  },
+
   verification: {
-    google: "8lf7yFKYW3BrCgdlVOzLZP967z_uCQE3N1KzAuQD358",
+    google: "8lf7yFKYW3BrCgd1VOzLZP967Z_uCQE3N1KzAuQD358",
     other: {
       "naver-site-verification": "61c50b0d437b1bc55810319a2ec71b5a0ea65c9d",
     },
