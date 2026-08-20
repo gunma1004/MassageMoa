f7f76d5bab391266939af122e6ee33e33155c124
gunma1004 <hanabank94@naver.com>
2026-08-20 14:12:52 +0900

    fix: enable static export in next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // 👈 이 줄이 out 폴더를 생성해 줍니다!
+  images: {
+    unoptimized: true, // 정적 export 시 이미지 에러 방지
+  },
 };
 
 export default nextConfig;
