0b4427a145ec3c34f18d06c5d74a66a5ec861d67
gunma1004 <hanabank94@naver.com>
2026-08-20 12:40:35 +0900

    키워드 변경

--- app/robots.ts
+++ app/robots.ts
@@ -2,7 +2,7 @@ import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
   // 🌐 마사지모아 새 도메인 주소로 입력해주세요 (예: https://massagemoa.com 또는 https://www.massagemoa.com)
-  const baseUrl = 'https://massagemoa.com';
+  const baseUrl = https://massagemoa.pages.dev ';
 
   return {
     rules: {
