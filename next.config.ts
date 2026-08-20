d0d1bc211152b120b08e0a35e945103cb6efaf97
gunma1004 <hanabank94@naver.com>
2026-08-20 14:34:01 +0900

    fix: set output export in next config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export', // 👈 이 줄이 out 폴더를 생성해 줍니다!
+  output: 'export',
   images: {
-    unoptimized: true, // 정적 export 시 이미지 에러 방지
+    unoptimized: true,
   },
 };
 
